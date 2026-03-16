d142464c039b83e988522a051d2ed046a5aecbf4
Bryant Azraqi <azraqibryant@gmail.com>
2026-03-17 02:14:16 +0700

    testing midtrans

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import express, { type Request, type Response } from 'express';
-import { checkout } from './controllers/orderController';
-import { midtransWebhook } from './controllers/webhookController';
+import { createSnapTransaction } from './services/midtransService';
 
 const app = express();
 app.use(express.json());
 
 app.get('/', (_req: Request, res: Response) => {
-  res.json({ status: 'ok', step: 'both-controllers' });
+  res.json({ status: 'ok', step: 'midtrans-imported' });
 });
 
 export default app;
