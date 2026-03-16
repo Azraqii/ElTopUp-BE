6e406d9ae16966c322dcef7f569c5dc8cd2b9e5e
Bryant Azraqi <azraqibryant@gmail.com>
2026-03-17 02:21:03 +0700

    Testing all controllers

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import express, { type Request, type Response } from 'express';
 import { checkout, getOrderStatus, getMyOrders, mockPayOrder } from './controllers/orderController';
 import { register, login, syncProfile, getMe } from './controllers/authController';
+import { midtransWebhook, robuxshipWebhook } from './controllers/webhookController';
 import { requireAuth } from './middleware/authMiddleware';
 
 const app = express();
 app.use(express.json());
 
 app.get('/', (_req: Request, res: Response) => {
-  res.json({ status: 'ok', step: 'auth-order-only' });
+  res.json({ status: 'ok', step: 'all-three-controllers' });
 });
 
 export default app;
