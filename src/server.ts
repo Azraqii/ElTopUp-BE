89ac864e33b3cda6716fbfa6575c4fe20ee4a460
Bryant Azraqi <azraqibryant@gmail.com>
2026-03-17 02:01:43 +0700

    Check if its webhook or routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express, { type Request, type Response } from 'express';
-import { checkout, getOrderStatus, getMyOrders, mockPayOrder } from './controllers/orderController';
+import orderRoutes from './routes/orderRoutes';
+import webhookRoutes from './routes/webhookRoutes';
 
 const app = express();
 app.use(express.json());
 
 app.get('/', (_req: Request, res: Response) => {
-  res.json({ status: 'ok', step: 'ordercontroller-only' });
+  res.json({ status: 'ok', step: 'order-and-webhook' });
 });
+
+app.use('/api/orders', orderRoutes);
+app.use('/api/webhooks', webhookRoutes);
 
 export default app;
